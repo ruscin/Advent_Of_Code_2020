94faa1696328ac51b9d81c5b7c6f2cb2dfcef696
Kacper Ruściński <ruscin13@gmail.com>
2020-12-29 16:33:43 +0100

    Code cleanup

--- day2/script.js
+++ day2/script.js
@@ -1,45 +1,33 @@
 const fs = require("fs");
 const input = fs.readFileSync("./ex2.txt", "utf-8").split("\r\n");
-// const input = [
-//     "1-3 a: abcde",
-// "1-3 b: cdefg",
-// '2-9 c: ccccccccc'
-// ]
 
 let counter = 0;
-let counter2 = 1
+let counter2 = 0;
 
 for (let i = 0; i < input.length; i++) {
   const splitedInput = input[i].split(" ");
   const [min, max] = splitedInput[0].split("-");
   const letter = splitedInput[1].charAt(0);
   const password = splitedInput[2];
-  const passwordArray = [...password]
+  const passwordArray = [...password];
 
-  const howManyTimesThereIsLetter = 
-  passwordArray.filter((el) => el === letter).length;
+  const letterOccurences = passwordArray.filter((el) => el === letter).length;
 
-  if (howManyTimesThereIsLetter >= min && howManyTimesThereIsLetter <= max) 
-  {
+  if (letterOccurences >= min && letterOccurences <= max) {
     counter++;
   }
 
   //PART 2
+  const minLetter = passwordArray[min - 1];
+  const maxLetter = passwordArray[max - 1];
 
-  if ((passwordArray[min-1] === letter && passwordArray[max-1] !== letter) 
-  || (passwordArray[min-1] !== letter && passwordArray[max-1] === letter) 
-  && passwordArray[min-1] !== passwordArray[max-1])
-  {
-      counter2++
+  if (
+    (minLetter === letter && maxLetter !== letter) ||
+    (minLetter !== letter && maxLetter === letter)
+  ) {
+    counter2++;
   }
 }
 
 console.log(counter);
 console.log(counter2);
-
-
-
-
-
-
-
