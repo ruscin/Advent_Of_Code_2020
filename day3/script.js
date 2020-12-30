7d2f6b1012bc07c9513b4bdf97596c651af0480f
Kacper Ruściński <ruscin13@gmail.com>
2020-12-30 10:38:31 +0100

    add not working day3 part 2

--- day3/script.js
+++ day3/script.js
@@ -1,15 +1,49 @@
 const fs = require("fs");
 const input = fs.readFileSync("./ex3.txt", "utf-8").split("\r\n");
-const repetedInput = [];
+const repeatedInput = [];
 const moveRight = 3;
-const treeCounter = 0
-input.forEach((el) => repetedInput.push(el.repeat(1200)));
+let treeCounter = 0;
+const treeCounter2 = [];
 
-for (let i = 0; i < repetedInput.length; i++) {
-  const aaa = [...repetedInput[i]][moveRight * i ]
-  if (aaa === "#"){
-    treeCounter++
+const slopePath = [
+  { x: 1, y: 1 },
+  { x: 3, y: 1 },
+  { x: 5, y: 1 },
+  { x: 7, y: 1 },
+  { x: 1, y: 2 },
+];
+
+input.forEach((el) => repeatedInput.push(el.repeat(100))); // 75 should be enough for this exercise, because 7 * 323 / 31 == 72
+
+for (let i = 0; i < repeatedInput.length; i++) {
+  const horizontalAxis = [...repeatedInput[i]][moveRight * i];
+  
+  if (horizontalAxis === "#") {
+    treeCounter++;
   }
 }
 
-console.log(treeCounter)
+// PART 2
+let helperCounter = 0;
+
+for (let i = 0; i < slopePath.length; i++) {
+  for (let j = 0; j < repeatedInput.length; j++) {
+    const horizontalAxis =
+      j % slopePath[i].y === 0
+        ? [...repeatedInput[j]][slopePath[i].x * j]
+        : null;
+
+    if (horizontalAxis === "#") {
+      helperCounter++;
+    }
+  }
+
+  treeCounter2[i] = helperCounter;
+  helperCounter = 0;
+}
+
+const finalResult = treeCounter2.reduce((acc, val) => acc * val, 1);
+
+console.log(treeCounter);
+console.log(treeCounter2);
+console.log(finalResult);
